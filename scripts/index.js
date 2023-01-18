9d96da976d6f69756491efefee7f57b95e31f2d2
GovnocodedByChapo <aoufchapo@gmail.com>
2023-01-18 20:36:37 +0300

    2

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
 
 
 const buttons = [
-    {text: 'VK', url: 'https://vk.com/ya_chapo'},
-    {text: 'Группа VK', url: 'https://vk.com/chaposcripts'},
-    {text: 'Telegram', url: 'https://telegram.me/ya_chapo'},
-    {text: 'Discord', url: 'https://discord.com/invite/4fNcFYE6WP'},
-    {text: 'Профиль на BlastHack', url: 'https://www.blast.hk/members/112329/'},
-    {text: 'GitHub', url: 'https://github.com/GovnocodedByChapo?tab=repositories'},
+    {short: 'vk', text: 'VK', url: 'https://vk.com/ya_chapo'},
+    {short: 'vk_group', text: 'Группа VK', url: 'https://vk.com/chaposcripts'},
+    {short: 'tg', text: 'Telegram', url: 'https://telegram.me/ya_chapo'},
+    {short: 'discord', text: 'Discord', url: 'https://discord.com/invite/4fNcFYE6WP'},
+    {short: 'bh', text: 'Профиль на BlastHack', url: 'https://www.blast.hk/members/112329/'},
+    {short: 'github', text: 'GitHub', url: 'https://github.com/GovnocodedByChapo?tab=repositories'},
 ]
+
+
 
 function openUrl(url) {
     document.location.href = url;
@@ -36,8 +38,15 @@ function createParticle(count = 50) {
     }
 }
 
+const domain = 'https://govnocodedbychapo.github.io/'
+
 const main = () => {
     //alert('load')
     drawButtons();
-    createParticle()
+    createParticle();
+    for (const item of buttons) {
+        if (document.locations.endsWith(item.short)) {
+            openUrl(item.url)
+        }
+    }
 }
